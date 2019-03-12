bcb3e2879b7b2a4785dd6038d650235424b830aa
Georgy Baranov <floridavbv@gmail.com>
2019-03-11 23:00:04 -0600

    portfolio test data

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Test' });
+});
+
+router.get('/resume', function(req, res, next) {
+  res.render('index', { title: 'Test' });
+});
+
+router.get('/portfolio', function(req, res, next) {
+  res.render('portfolio', { title: 'Test' });
 });
 
 module.exports = router;
